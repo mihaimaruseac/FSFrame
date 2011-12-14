122baccf0d0e78e48b262a0de71776c47aaa96da
Mihai Maruseac <mihai@rosedu.org>
2011-12-14 07:30:15 +0200

    Add initial stub files

--- Frame.hs
+++ Frame.hs
@@ -2,5 +2,11 @@ module Frame
   (module Frame.GUI
   ) where
 
+{-
+This modules is used to reexport the `frameGUI` function such that `Main.hs`
+can see it and declare it to be the main entry point of the `frame`
+application.
+-}
+
 import Frame.GUI (frameGUI)
 
---
+++ Frame/GUI.hs
@@ -0,0 +1,15 @@
+module Frame.GUI
+where
+
+{-
+This modules is used to construct the GUI of the `frame` application. It
+contains all functions needed to build the GUI and interact with it but it
+exports only the main building function to the outside.
+-}
+
+{-
+The main GUI building function.
+-}
+frameGUI :: IO ()
+frameGUI = print "ok"
+
