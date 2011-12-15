6fc42a909cb5f4f70e92576ee6d70d24a06c5084
Mihai Maruseac <mihai@rosedu.org>
2011-12-15 08:56:30 +0200

    Add initial FCREATE stub.

--- Frame/FrameOps.hs
+++ Frame/FrameOps.hs
@@ -6,5 +6,20 @@ This module holds the definitions for each operation working on a frame or the
 entire world.
 -}
 
+import Control.Monad.State (modify, gets, State)
+
 import Frame.Types
+
+{-
+Evaluates a `FCREATE` command.
+-}
+fcreate :: String -> FrameType -> [String] -> State FSState ()
+fcreate name typ parents = do
+  -- TODO: check for collisions
+  -- 1. Build one frame
+  let f = Frame name typ [] [] []
+  -- TODO: update list of parents
+  -- TODO: update parents' list of children
+  -- 2. Add frame to world
+  modify (\(w, p) -> (f:w, p))
 
