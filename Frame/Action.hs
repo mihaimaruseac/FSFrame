b374a6188c742d31482fd21c54ecfb1ae9b7b123
Mihai Maruseac <mihai@rosedu.org>
2011-12-18 12:15:39 +0200

    Update action stubs to take into account user preferences.

--- Frame/Action.hs
+++ Frame/Action.hs
@@ -5,6 +5,7 @@ where
 This module implements needed functionality for actions in the frame system.
 -}
 
+import Control.Monad
 import Control.Monad.Trans.State.Strict --(modify, gets, State)
 
 import Frame.Types
@@ -12,6 +13,8 @@ import Frame.Types
 {-
 Execute a simple action. TODO: define it.
 -}
-executeAction :: Action -> State FSState Obj
-executeAction = undefined
+executeAction :: Pref -> Action -> State FSState Obj
+executeAction p a = do
+  unless (prefActionsEnabled p) $ error "Action is required but disabled."
+  return undefined
 
